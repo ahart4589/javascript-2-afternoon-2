d1df52c5c1c957731f29022d773c117e294c5862
Alexandria Hart <alexandriahart@moondog.local>
2018-07-10 16:41:39 -0600

    finished practice problems

--- practice.js
+++ practice.js
@@ -34,7 +34,9 @@ var arr = [40,50,60];
   Return the last item in the given array.
 */
 
-//Code Here
+function last(arr){
+  return arr[2];
+}
 
 
 
@@ -49,8 +51,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
   Loop through the given array and alert every item in the array.
 */
 
-//Code Here
-
+function looper(family) {
+  for(let i=0;i<family.length;i++){
+    alert(family[i]);
+  }
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -64,7 +69,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop through the given array backwards alerting every item in the array starting at the end.\
 */
 
-//Code Here
+function reversedLooper(letters){
+  for(let i=letters.length-1;i>=0;i--){
+    alert(letters[i]);
+  }
+}
 
 
 
@@ -79,7 +88,14 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
   Return an array that contains the even numbers from the nums array.
 */
 
-//Code Here
+function evenFinder(nums){
+  let evenNums = [];
+  for(let i=0;i<nums.length;i++){
+    if(nums[i]%2===0){
+      evenNums.push(nums[i]);
+    }
+  }return evenNums;
+}
 
 
 
